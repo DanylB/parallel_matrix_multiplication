ec0ab66c7b4bed38107d957423524553572f82ac
DanylB <bbogomedov5@gmail.com>
2021-02-26 01:24:34 +0300

    hard code, but it works

--- main.go
+++ main.go
@@ -6,104 +6,173 @@ import (
 	"time"
 )
 
-const columnArrA int = 6
-const rowArrA int = 7
+const rowArrA int = 6
+const columnArrA int = 7
 
-const columnArrB int = 7
-const rowArrB int = 8
+const rowArrB int = 7
+const columnArrB int = 8
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	var a = generateArrayA()
-	var b = generateArrayB()
+	// var a = generateArrayA()
+	// var b = generateArrayB()
 
-	// var a = [2][3]int{{1, 1}, {2, 2, 2}}
-	// var b = [3][4]int{{1, 1, 1}, {2, 2, 2, 2}}
+	var a = [6][7]int{
+		{1, 2, 3, 4, 4, 3, 2},
+		{4, 5, 6, 6, 6, 5, 4},
+		{4, 5, 6, 7, 7, 6, 5},
+		{1, 2, 3, 4, 4, 3, 2},
+		{4, 5, 6, 6, 6, 5, 4},
+		{4, 5, 6, 7, 7, 6, 5},
+	}
+
+	var b = [7][8]int{
+		{1, 2, 3, 4, 4, 3, 2, 1},
+		{4, 5, 6, 6, 6, 5, 4, 3},
+		{4, 5, 6, 7, 7, 6, 5, 4},
+		{1, 2, 3, 4, 4, 3, 2, 1},
+		{4, 5, 6, 6, 6, 5, 4, 3},
+		{4, 5, 6, 7, 7, 6, 5, 4},
+		{4, 5, 6, 7, 7, 6, 5, 4},
+	}
 
 	printArrayA(a)
 	printArrayB(b)
 
-	var resultArray = addArrays(a, b)
+	// var resultArray = addArrays(a, b)
 
-	printRessultArray(resultArray)
+	// printRessultArray(resultArray)
+
+	addArrays(a, b)
 
 }
 
-func generateArrayA() [columnArrA][rowArrA]int {
-	var a = [columnArrA][rowArrA]int{}
-	for i := 0; i < columnArrA; i++ {
-		for j := 0; j < rowArrA; j++ {
+func generateArrayA() [rowArrA][columnArrA]int {
+	var a = [rowArrA][columnArrA]int{}
+	for i := 0; i < rowArrA; i++ {
+		for j := 0; j < columnArrA; j++ {
 			a[i][j] = rand.Intn(1000) + 1
 		}
 	}
 	return a
 }
 
-func generateArrayB() [columnArrB][rowArrB]int {
-	var b = [columnArrB][rowArrB]int{{}, {}}
-	for i := 0; i < columnArrB; i++ {
-		for j := 0; j < rowArrB; j++ {
+func generateArrayB() [rowArrB][columnArrB]int {
+	var b = [rowArrB][columnArrB]int{{}, {}}
+	for i := 0; i < rowArrB; i++ {
+		for j := 0; j < columnArrB; j++ {
 			b[i][j] = rand.Intn(1000) + 1
 		}
 	}
 	return b
 }
 
-func printArrayA(a [columnArrA][rowArrA]int) {
+func printArrayA(a [rowArrA][columnArrA]int) {
 	print("\n====================== Array A ===================\n")
 
-	for i := 0; i < columnArrA; i++ {
-		for j := 0; j < rowArrA; j++ {
+	for i := 0; i < rowArrA; i++ {
+		for j := 0; j < columnArrA; j++ {
 			fmt.Printf("%d\t", a[i][j])
 		}
 		print("\n")
 	}
 }
-func printArrayB(b [columnArrB][rowArrB]int) {
+func printArrayB(b [rowArrB][columnArrB]int) {
 	print("\n========================= Array B ========================\n")
-	for i := 0; i < columnArrB; i++ {
-		for j := 0; j < rowArrB; j++ {
+	for i := 0; i < rowArrB; i++ {
+		for j := 0; j < columnArrB; j++ {
 			fmt.Printf("%d\t", b[i][j])
 		}
 		print("\n")
 	}
 }
 
-func printRessultArray(resultArray [columnArrA][rowArrB]int) {
+func printRessultArray(resultArray [rowArrA][columnArrB]int) {
 	print("\n========================= RESULT Array ========================\n")
-	for i := 0; i < columnArrA; i++ {
-		for j := 0; j < rowArrB; j++ {
+	for i := 0; i < rowArrA; i++ {
+		for j := 0; j < columnArrB; j++ {
 			fmt.Printf("%d\t", resultArray[i][j])
 		}
 		print("\n")
 	}
 }
 
-func addArrays(a [columnArrA][rowArrA]int, b [columnArrB][rowArrB]int) [columnArrA][rowArrB]int {
+// func addArrays(a [columnArrA][rowArrA]int, b [columnArrB][rowArrB]int) [columnArrA][rowArrB]int {
+// 	var i int
+// 	var j int
+// var k int
+//
+// 	var bt [8][7]int
+// 	var res [6][8]int
+//
+// 	for i = 0; i < 8; i++ {
+// 		for j = 0; j < 7; j++ {
+// 			bt[i][j] = b[j][i]
+//
+// 		}
+// 	}
+//
+// 	for i = 0; i < 6; i++ {
+// 		for j = 0; j < 8; j++ {
+// 			res[i][j] = 0
+// 			for k = 0; k < 7; k++ {
+// 				res[i][j] = a[i][k] * bt[j][k]
+// 			}
+// 		}
+// 	}
+//
+// 	return res
+//
+// }
+
+func addArrays(a [rowArrA][columnArrA]int, b [rowArrB][columnArrB]int) {
+
+	addLine1(a, b)
+	addLine2(a, b)
+
+}
+
+func addLine1(a [rowArrA][columnArrA]int, b [rowArrB][columnArrB]int) {
+
 	var i int
 	var j int
 	var k int
 
-	var bt [8][7]int
-	var res [6][8]int
+	var res [1][8]int
 
-	for i = 0; i < 8; i++ {
-		for j = 0; j < 7; j++ {
-			bt[i][j] = b[j][i]
-
-		}
-	}
-
-	for i = 0; i < 6; i++ {
+	for i = 0; i < 1; i++ {
 		for j = 0; j < 8; j++ {
 			res[i][j] = 0
 			for k = 0; k < 7; k++ {
-				res[i][j] = a[i][k] * bt[j][k]
+				res[i][j] += a[i][k] * b[k][j]
 			}
+			print(res[i][j], " ")
 		}
+
 	}
+	print("\n")
+	return
+}
 
-	return res
+func addLine2(a [rowArrA][columnArrA]int, b [rowArrB][columnArrB]int) {
 
+	var i int
+	var j int
+	var k int
+
+	var res [2][8]int
+
+	for i = 1; i < 2; i++ {
+		for j = 0; j < 8; j++ {
+			res[i][j] = 0
+			for k = 0; k < 7; k++ {
+				res[i][j] += a[i][k] * b[k][j]
+			}
+			print(res[i][j], " ")
+		}
+
+	}
+	print("\n")
+	return
 }
---
+++ main_test.go
@@ -0,0 +1,301 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+const N = 200000000
+
+const rowArrA int = 6
+const columnArrA int = 7
+const rowArrB int = 7
+const columnArrB int = 8
+
+var a = [rowArrA][columnArrA]int{}
+var arrA = &a
+var b = [rowArrB][columnArrB]int{}
+var arrB = &b
+
+var res1 = [1][8]int{}
+var resArr1 = &res1
+var res2 = [2][8]int{}
+var resArr2 = &res2
+var res3 = [3][8]int{}
+var resArr3 = &res3
+var res4 = [4][8]int{}
+var resArr4 = &res4
+var res5 = [5][8]int{}
+var resArr5 = &res5
+var res6 = [6][8]int{}
+var resArr6 = &res6
+var res7 = [7][8]int{}
+var resArr7 = &res7
+
+var wg sync.WaitGroup
+
+// func main() {
+func BenchmarkCount(bench *testing.B) {
+
+	runtime.GOMAXPROCS(8)
+
+	rand.Seed(time.Now().UnixNano())
+
+	wg.Add(2)
+	go generateArrayA()
+	go generateArrayB()
+	wg.Wait()
+
+	printArrayA()
+	printArrayB()
+
+	addArrays()
+
+	printRessultArray()
+
+}
+
+func generateArrayA() {
+	for i := 0; i < rowArrA; i++ {
+		for j := 0; j < columnArrA; j++ {
+			a[i][j] = rand.Intn(10) + 1
+		}
+	}
+	*arrA = a
+	wg.Done()
+	return
+}
+
+func generateArrayB() {
+	for i := 0; i < rowArrB; i++ {
+		for j := 0; j < columnArrB; j++ {
+			b[i][j] = rand.Intn(10) + 1
+		}
+	}
+	*arrB = b
+	wg.Done()
+	return
+
+}
+
+func addArrays() {
+	wg.Add(7)
+
+	go addLine1()
+	go addLine2()
+	go addLine3()
+	go addLine4()
+	go addLine5()
+	go addLine6()
+	go addLine7()
+	wg.Wait()
+
+}
+
+//
+// func count() {
+// 	var d = 0
+// 	for e := 0; e < 200000000; e++ {
+// 		d += e
+// 	}
+// 	wg.Done()
+// }
+
+func addLine1() {
+
+	var res [1][8]int
+	for i := 0; i < 1; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr1 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine2() {
+	var res [2][8]int
+	for i := 1; i < 2; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr2 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine3() {
+	var res [3][8]int
+	for i := 2; i < 3; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr3 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine4() {
+	var res [4][8]int
+	for i := 3; i < 4; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr4 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine5() {
+	var res [5][8]int
+	for i := 4; i < 5; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr5 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine6() {
+	var res [6][8]int
+	for i := 5; i < 6; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr6 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+func addLine7() {
+	var res [7][8]int
+	for i := 6; i < 6; i++ {
+		for j := 0; j < 8; j++ {
+			res[i][j] = 0
+			for k := 0; k < 7; k++ {
+				res[i][j] += arrA[i][k] * arrB[k][j]
+			}
+			*resArr7 = res
+		}
+	}
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
+	wg.Done()
+	return
+}
+
+func printArrayA() {
+	print("\n====================== Array A ===================\n")
+
+	for i := 0; i < rowArrA; i++ {
+		for j := 0; j < columnArrA; j++ {
+			fmt.Printf("%d\t", a[i][j])
+		}
+		print("\n")
+	}
+}
+func printArrayB() {
+	print("\n========================= Array B ========================\n")
+	for i := 0; i < rowArrB; i++ {
+		for j := 0; j < columnArrB; j++ {
+			fmt.Printf("%d\t", b[i][j])
+		}
+		print("\n")
+	}
+}
+
+func printRessultArray() {
+
+	print("\n========================= RESULT Array ========================\n")
+	for i := 0; i < 1; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res1[i][j])
+		}
+		print("\n")
+	}
+	for i := 1; i < 2; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res2[i][j])
+		}
+		print("\n")
+	}
+	for i := 2; i < 3; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res3[i][j])
+		}
+		print("\n")
+	}
+	for i := 3; i < 4; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res4[i][j])
+		}
+		print("\n")
+	}
+	for i := 4; i < 5; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res5[i][j])
+		}
+		print("\n")
+	}
+	for i := 5; i < 6; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res6[i][j])
+		}
+		print("\n")
+	}
+	for i := 6; i < 6; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("%d\t", res7[i][j])
+		}
+		print("\n")
+	}
+}
