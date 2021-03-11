79c28e370774713a8dfa4e849deb0721d7db83d1
DanylB <bbogomedov5@gmail.com>
2021-03-11 20:38:01 +0300

    final

--- main_test.go
+++ main_test.go
@@ -9,7 +9,8 @@ import (
 	"time"
 )
 
-const N = 200000000
+const N int = 200000000
+const coresNum int = 8
 
 const rowArrA int = 6
 const columnArrA int = 7
@@ -21,27 +22,27 @@ var arrA = &a
 var b = [rowArrB][columnArrB]int{}
 var arrB = &b
 
-var res1 = int{}
+var res1 = [1][columnArrB]int{}
 var resArr1 = &res1
-var res2 = int{}
+var res2 = [2][columnArrB]int{}
 var resArr2 = &res2
-var res3 = int{}
+var res3 = [3][columnArrB]int{}
 var resArr3 = &res3
-var res4 = int{}
+var res4 = [4][columnArrB]int{}
 var resArr4 = &res4
-var res5 = int{}
+var res5 = [5][columnArrB]int{}
 var resArr5 = &res5
-var res6 = int{}
+var res6 = [6][columnArrB]int{}
 var resArr6 = &res6
-var res7 = int{}
+var res7 = [7][columnArrB]int{}
 var resArr7 = &res7
 
 var wg sync.WaitGroup
 
-// func main() {
+// BenchmarkCount  func main() {
 func BenchmarkCount(bench *testing.B) {
 
-	runtime.GOMAXPROCS(8)
+	runtime.GOMAXPROCS(coresNum)
 
 	rand.Seed(time.Now().UnixNano())
 
@@ -84,7 +85,6 @@ func generateArrayB() {
 
 func addArrays() {
 	wg.Add(6)
-
 	go addLine1()
 	go addLine2()
 	go addLine3()
@@ -92,120 +92,100 @@ func addArrays() {
 	go addLine5()
 	go addLine6()
 	wg.Wait()
-
 }
 
 func addLine1() {
-
-	var res [1][8]int
+	var res [1][columnArrB]int
 	for i := 0; i < 1; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr1 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
 
 func addLine2() {
-	var res [2][8]int
+	var res [2][columnArrB]int
 	for i := 1; i < 2; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr2 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
 
 func addLine3() {
-	var res [3][8]int
+	var res [3][columnArrB]int
 	for i := 2; i < 3; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr3 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
 
 func addLine4() {
-	var res [4][8]int
+	var res [4][columnArrB]int
 	for i := 3; i < 4; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr4 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
 
 func addLine5() {
-	var res [5][8]int
+	var res [5][columnArrB]int
 	for i := 4; i < 5; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr5 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
 
 func addLine6() {
-	var res [6][8]int
+	var res [6][columnArrB]int
 	for i := 5; i < 6; i++ {
-		for j := 0; j < 8; j++ {
+		for j := 0; j < columnArrB; j++ {
 			res[i][j] = 0
-			for k := 0; k < 7; k++ {
+			for k := 0; k < rowArrB; k++ {
 				res[i][j] += arrA[i][k] * arrB[k][j]
 			}
 			*resArr6 = res
 		}
 	}
-	var d = 0
-	for e := 0; e < N; e++ {
-		d += e
-	}
+	myBench()
 	wg.Done()
 	return
 }
@@ -234,27 +214,27 @@ func printRessultArray() {
 
 	print("\n========================= RESULT Array ========================\n")
 
-	for j := 0; j < 8; j++ {
+	for j := 0; j < columnArrB; j++ {
 		fmt.Printf("%d\t", res1[0][j])
 	}
 	print("\n")
 
-	for j := 0; j < 8; j++ {
+	for j := 0; j < columnArrB; j++ {
 		fmt.Printf("%d\t", res2[1][j])
 	}
 	print("\n")
 
-	for j := 0; j < 8; j++ {
+	for j := 0; j < columnArrB; j++ {
 		fmt.Printf("%d\t", res3[2][j])
 	}
 	print("\n")
 
-	for j := 0; j < 8; j++ {
+	for j := 0; j < columnArrB; j++ {
 		fmt.Printf("%d\t", res4[3][j])
 	}
 	print("\n")
 
-	for j := 0; j < 8; j++ {
+	for j := 0; j < columnArrB; j++ {
 		fmt.Printf("%d\t", res5[4][j])
 	}
 	print("\n")
@@ -263,4 +243,11 @@ func printRessultArray() {
 		fmt.Printf("%d\t", res6[5][j])
 	}
 	print("\n\n")
+}
+
+func myBench() {
+	var d = 0
+	for e := 0; e < N; e++ {
+		d += e
+	}
 }
